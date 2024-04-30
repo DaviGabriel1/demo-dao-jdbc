991fd41a6dd506511dea217e8ce9fa45530256f2
Davi Gabriel <davig41441503@gmail.com>
2024-04-30 10:11:40 -0300

    criacao da classe Department

---
+++ src/main/java/model/entities/Department.java
@@ -0,0 +1,56 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {//permite identificar objetos mesmo sendo transformados em bytes
+    private Integer id;
+    private String name;
+    private static final long serialVersionUID = 1L;
+
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
